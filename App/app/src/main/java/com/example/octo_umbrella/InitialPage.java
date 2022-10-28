029f0071720869ff36dddd372797dc711b25b466
wellingtonmacena <wellington.macena.23@gmail.com>
2022-10-28 01:43:54 -0300

    Added Initial Page

---
+++ App/app/src/main/java/com/example/octo_umbrella/InitialPage.java
@@ -0,0 +1,26 @@
+package com.example.octo_umbrella;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class InitialPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_page);
+    }
+
+    public void GoToSignUpPage(View view) {
+        Intent i = new Intent(InitialPage.this, SignupActivity.class);
+        startActivity(i);
+    }
+
+    public void GoToLoginPage(View view) {
+        Intent i = new Intent(InitialPage.this, LoginActivity.class);
+        startActivity(i);
+    }
+}
--- App/app/src/main/java/com/example/octo_umbrella/LoadingActivity.java
+++ App/app/src/main/java/com/example/octo_umbrella/LoadingActivity.java
@@ -2,7 +2,9 @@ package com.example.octo_umbrella;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -11,5 +13,16 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_activity);
 
+        var runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LoadingActivity.this, InitialPage.class);
+                startActivity(i);
+                finish();
+            }
+        };
+
+        var handlerThread = new Handler();
+        handlerThread.postDelayed(runnable, 500);
     }
 }
