6d973cbe8a9a8c20bea31ea98c5f1f8157ad8673
wellingtonmacena <wellington.macena.23@gmail.com>
2022-10-29 23:13:38 -0300

    Added login page

--- App/app/src/main/java/com/example/octo_umbrella/LoginActivity.java
+++ App/app/src/main/java/com/example/octo_umbrella/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.octo_umbrella;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void Enter(View view) {
+    }
+
+    public void CreateAccount(View view) {
+        Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+        startActivity(i);
     }
 }
--- App/app/src/main/java/com/example/octo_umbrella/SignupActivity.java
+++ App/app/src/main/java/com/example/octo_umbrella/SignupActivity.java
@@ -3,7 +3,19 @@ package com.example.octo_umbrella;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -14,5 +26,54 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     public void SignUp(View view) {
+
+        loadDogImage();
+    }
+
+    // function for making a HTTP request using Volley and
+    // inserting the image in the ImageView using Glide library
+    private void loadDogImage() {
+
+        // getting a new volley request queue for making new requests
+        RequestQueue volleyQueue = Volley.newRequestQueue(SignupActivity.this);
+        // url of the api through which we get random dog images
+        String url = "https://octo-umbrella-api.herokuapp.com/user";
+
+        // since the response we get from the api is in JSON, we
+        // need to use `JsonObjectRequest` for parsing the
+        // request response
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                // we are using GET HTTP request method
+                Request.Method.GET,
+                // url we want to send the HTTP request to
+                url,
+                // this parameter is used to send a JSON object to the
+                // server, since this is not required in our case,
+                // we are keeping it `null`
+                null,
+
+                // lambda function for handling the case
+                // when the HTTP request succeeds
+                (Response.Listener<JSONObject>) response -> {
+                    // get the image url from the JSON object
+                    Log.e("MainActivity", response.toString());
+                },
+
+                // lambda function for handling the case
+                // when the HTTP request fails
+                (Response.ErrorListener) error -> {
+                    // make a Toast telling the user
+                    // that something went wrong
+                    Toast.makeText(SignupActivity.this, "Some error occurred! Cannot fetch dog image", Toast.LENGTH_LONG).show();
+                    // log the error message in the error stream
+                    Log.e("MainActivity", "loadDogImage error: ${error.localizedMessage}");
+                }
+        );
+
+        // add the json request object created above
+        // to the Volley request queue
+        volleyQueue.add(jsonObjectRequest);
+
+        var f = jsonObjectRequest;
     }
 }
